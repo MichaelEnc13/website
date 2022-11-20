576cb76e7fddef28c212cf4ea8e0b87b0f846896
Steven <franciscomiguel1315@gmail.com>
2022-11-20 13:14:54 -0400

    Desarrolle la vista de login + estilos, vista del dashboard + estilos, agregue los iconos box-icon

---
+++ views/dashboard/dashboard.php
@@ -0,0 +1,132 @@
+<section class="container__dashboard">
+    <div class="menu__dash">
+        <h3 class="title__dash">Dotsell Solutions</h3>
+        <button class="btn__exit">Exit</button>
+    </div>
+    <div class="container__dash">
+        <div class="nav__dashboard">
+            <div class="btn__nav__dash">
+                <button>Sistemas</button>
+                </div>
+            <div class="btn__nav__dash">
+                <button>Agregar Sistemas</button>
+                </div>
+        </div>
+        <div class="sistem__dashboard">
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre de sistem</h3>
+            </div>
+            <div class="container__system">
+                <div class="info__system">
+                    <img class="img__system" src="src/img/logo.jpg" alt="">
+                    <div class="icon__system">
+                    <i class='bx bxs-edit-alt'></i>
+                    <i class='bx bxs-trash'></i>
+                    </div>
+                </div>
+                <h3 class="name__system">Nombre del sistem</h3>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="src/libs/jquery.min.js"></script>
+<script src="src/libs/sweetAlert.js"></script>
+<script src="src/libs/all.min.js"></script>
+<script src="src/js/script.js"></script>
---
+++ views/login/login.php
@@ -0,0 +1,11 @@
+
+<section class="container__login">
+    <div class="login">
+        <h1 class="title__login">Dotsell Solutions</h1>
+        <div class="group__input">
+            <input type="text" class="input__login" placeholder="Usuario">
+            <input type="password" class="input__login" placeholder="Password">
+        </div>
+        <button class="btn__login">Entrar</button>
+    </div>
+</section>
