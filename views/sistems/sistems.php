908a36f2aa4131f0acec261545e27d2af34765ec
Michael Encarnacion <maicol132603@gmail.com>
2022-11-26 17:05:40 -0400

    Cambios en la sección de sistemas

--- views/sistems/sistems.php
+++ views/sistems/sistems.php
@@ -1,30 +1,31 @@
-<?php
+<div class="sistems__container">
+    <?php
 
-include file_exists("../../post_creator/model/autoload") ? "../../post_creator/model/autoload" : "post_creator/model/autoload.php";
-$posts = Poster::get_all_post()['data'];
- 
-foreach ($posts as $post) :
-    $img = json_decode($post['imgs'],true);
-    $img_name = $img['img_1']['name'];
-    $img_uri = "src/uploads/post_".$post['id']."/".$post['id']."_img_".$img_name;
-   // var_dump($img['img_1']['name'])
-   
-?>
+    include file_exists("../../post_creator/model/autoload") ? "../../post_creator/model/autoload" : "post_creator/model/autoload.php";
+    $posts = Poster::get_all_post()['data'];
 
-    <div class="sistems__container">
+    foreach ($posts as $post) :
+        $img = json_decode($post['imgs'], true);
+        $img_name = $img['img_1']['name'];
+        $img_uri = "src/uploads/post_" . $post['id'] . "/" . $post['id'] . "_img_" . $img_name;
+        // var_dump($img['img_1']['name'])
+
+    ?>
+
+
         <section class="sistems">
             <div class="sistem__pic">
-                <img src="<?php echo $img_uri?>" alt="Foto del sistema">
+                <img src="<?php echo $img_uri ?>" alt="Foto del sistema">
             </div>
             <div class="sistem__info">
                 <h2 class="sistem__info__title"><?php echo $post['title'] ?></h2>
                 <div class="sistem__info__description">
                     <?php echo $post['description'] ?>
                 </div>
-                <button class="btn read_post"   data-id="<?php echo $post['id'] ?>">Ver más</button>
+                <button class="btn read_post" data-id="<?php echo $post['id'] ?>">Ver más</button>
             </div>
 
         </section>
-    </div>
 
-<?php endforeach; ?>
+    <?php endforeach; ?>
+</div>
