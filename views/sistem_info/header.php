f483340a1e9001398c20980f9efb6ffe251da24b
Steven <franciscomiguel1315@gmail.com>
2022-11-26 21:30:26 -0400

    Mostrar datos

--- views/sistem_info/header.php
+++ views/sistem_info/header.php
@@ -1,19 +1,28 @@
 
 <?php
     include file_exists("autoload.php") ? "autoload.php" : "post_creator/model/autoload.php";
-    $post = Poster::get_post($_GET['id'])['data']->fetch()['post_info'];
-    $post_info  = json_decode($post, true);
+    $posts = Poster::get_post($_GET['id'])['data']->fetch();
+    $datos= json_decode($posts['plan'], true);
     
+    $img = json_decode($posts['imgs'],true);
+    $img_name_1 = $img['img_1']['name'];
+    $img_name_2 = $img['img_2']['name'];
+    $img_name_3 = $img['img_3']['name'];
+    $img_name_4 = $img['img_4']['name'];
+    $img_1 = "src/uploads/post_".$posts['id']."/".$posts['id']."_img_".$img_name_1;
+    $img_2 = "src/uploads/post_".$posts['id']."/".$posts['id']."_img_".$img_name_2;
+    $img_3 = "src/uploads/post_".$posts['id']."/".$posts['id']."_img_".$img_name_3;
+    $img_4 = "src/uploads/post_".$posts['id']."/".$posts['id']."_img_".$img_name_4;
 
 
 ?>
 <header class="sistem_info_header">
     <div class="sistem_info_header_overlay"></div>
-    <img src="src/img/screenshots/seguros-client.png" alt="">
+    <img src="<?php echo $img_1?>" alt="">
 
     <div class="sistem_info_header_text">
-        <h1 class="sistem_name"><?php echo $post_info['title'] ?></h1>
-        <h3 class="sistem_info"><?php echo $post_info['subtitle'] ?></h3>
+        <h1 class="sistem_name"><?php echo $posts['title'] ?></h1>
+        <h3 class="sistem_info"><?php echo $posts['subtitle'] ?></h3>
     </div>
 
 </header>
